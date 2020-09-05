f5efa6a509799cd104798b1af642aeab357d0049
Blazing-Bonfire <chenyingfa1999@qq.com>
2020-09-06 04:40:29 +0800

    UI update

--- app/src/main/java/com/example/xinguannews/MainActivity.java
+++ app/src/main/java/com/example/xinguannews/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.xinguannews;
 
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
